500e99dc3041051bbcf317de1c95f01baf827a9e
Stas <staslz42stas@gmail.com>
2019-11-18 16:34:10 +0300

    обновлен в 0

--- src/myWebServer.h
+++ src/myWebServer.h
@@ -1,7 +1,9 @@
+//#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
+//#include <fs.h>
 #ifndef common
  #include "common.h"
 #endif 
@@ -13,9 +15,58 @@
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
+String getContentType(String filename) {
+  if (httpServer.hasArg("download")) return "application/octet-stream";
+  else if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+ }
+
+
+bool handleFileRead(String path) {  
+  if (path.endsWith("/")) path += "index.htm";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  /*
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = httpServer.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  */
+  return false;
+ }
+
+
+
+
+
 void MyWebinit(void){
   
   httpUpdater.setup(&httpServer);
+ 
+  
+
+
+  httpServer.onNotFound([]() {
+    if (!handleFileRead(httpServer.uri()))
+      httpServer.send(404, "text/plain", "FileNotFound");      
+  });
+
+
   httpServer.begin();
   //Serial.println(WiFi.localIP);
   Serial.print("HTTPUpdateServer ready! Open http://192.168.1.234/update in your browser\n");
--- src/myWiFi.h
+++ src/myWiFi.h
@@ -1,12 +1,14 @@
 #define myWiFi
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 
 #ifndef STASSID
 #define STASSID "SAN"
 #define STAPSK  "37212628"
 #endif
 
-IPAddress ip(192,168,1,234);  
-IPAddress gateway(192,168,1,1);
+IPAddress ip(192,168,0,234);  
+IPAddress gateway(192,168,0,1);
 IPAddress subnet(255,255,255,0);
 IPAddress dns1(194,158,196,137);
 IPAddress dns2(194,158,196,141);
@@ -16,7 +18,7 @@ const char* ssid = STASSID;
 const char* password = STAPSK;
 
 void MyWiFiInit(void){
- WiFi.mode(WIFI_AP_STA);
+  WiFi.mode(WIFI_AP_STA);
   WiFi.config(ip, gateway, subnet, dns1, dns2);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
